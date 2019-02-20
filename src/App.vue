21f79009cc0ea9f5210a344312484fe37a5f9776
Autumn Martin <autumnmartin@c3lx.com>
2019-02-20 14:11:08 -0700

    Add list component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Lister!"/>
+    <h2>Here's a List:</h2>
+    <List />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    List
   }
 }
 </script>
---
+++ src/components/List.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list">
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in list" :key='index'>
+          {{index + 1}}. {{data.item}}
+        </li>
+      </ul>
+    </div>
+    <h2 v-if="list.length >= 5">You have lots to do!</h2>
+    <h2 v-else>Don't you have work to do?</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      list: [
+          { "item": "find a puppy" },
+          { "item": "ski Crested Butte" },
+          { "item": "speak French" },
+          { "item": "learn this Vue thing"},
+          { "item": "catch a gopher"},
+          { "item": "go to the art museum"},
+          { "item": "see Guardians of the Galaxy, Vol 1 again"}
+      ]
+    }
+  },
+}
+</script>
