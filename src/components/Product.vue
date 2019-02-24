37cbe0dc1ac3d0086fdd7245bcb844257fa34f21
Autumn Martin <autumnmartin@c3lx.com>
2019-02-23 23:19:20 -0700

    Add product image

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,6 +1,28 @@
 
 <template>
-  <h2>Buy me a {{ product }}</h2>
+  <div class="product">
+    <h2>Buy me a {{ product }}</h2>
+
+    <div class="product-image">
+      <img src="../../public/seth-weisfeld-510833-unsplash.jpg" >
+    </div>
+    <div>
+      <a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+         href="https://unsplash.com/@jackharner?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+         target="_blank" rel="noopener noreferrer"
+         title="Download free do whatever you want high-resolution photos from Jack Harner">
+         <span style="display:inline-block;padding:2px 3px">
+           <svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32">
+             <title>unsplash-logo</title>
+             <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+           </svg>
+        </span>
+        <span style="display:inline-block;padding:2px 3px">
+          Jack Harner
+        </span>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -77,6 +99,7 @@ textarea {
 
 .product-image {
   width: 80%;
+  border-radius: 5px;
 }
 
 .product-image,
