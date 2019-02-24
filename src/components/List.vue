d7c6cbd3705ee1eccb0441337f804ff1a610f17c
Autumn Martin <autumnmartin@c3lx.com>
2019-02-23 21:23:37 -0700

    Change messages

--- src/components/List.vue
+++ src/components/List.vue
@@ -7,8 +7,8 @@
         </li>
       </ul>
     </div>
-    <h2 v-if="list.length >= 5">You have lots to do!</h2>
-    <h2 v-else>Don't you have work to do?</h2>
+    <h2 v-if="list.length >= 5">I have lots to do!</h2>
+    <h2 v-else>Don't I have work to do?</h2>
   </div>
 </template>
 
